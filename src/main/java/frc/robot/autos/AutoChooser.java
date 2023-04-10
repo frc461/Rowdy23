82de4f42a2af7f7c14e005e163e9e0629fb3578a
daniel <daniel@andymark.com>
2023-04-10 15:57:24 -0400

    forgot to save...

--- src/main/java/frc/robot/autos/AutoChooser.java
+++ src/main/java/frc/robot/autos/AutoChooser.java
@@ -1,3 +1,21 @@
+
+package frc.robot.autos;
+
+import java.util.HashMap;
+
+import com.pathplanner.lib.commands.FollowPathWithEvents;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.commands.AutoBalance;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Swerve;
