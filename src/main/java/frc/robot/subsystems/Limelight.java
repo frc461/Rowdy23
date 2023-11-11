63f86519bb932d8a214f8b3a0c67bbfe98c565a8
WilmKwn <williamkwon03@gmail.com>
2023-11-11 16:22:35 -0500

    Network Table Does Updates???

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -21,8 +21,11 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.Subscriber;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -32,48 +35,57 @@ import frc.robot.Constants;
 public class Limelight extends SubsystemBase{
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    private double botpose[];
-    private double botposefromtag[];
-    public double botPose2d[];
+    DoubleArraySubscriber ySub;
+    private double botPose[];
+    public double botPoseX;
+    public double botPoseZ;
+    public int updates;
 
     public Limelight() {
-        botPose2d = new double[2];
+        ySub = table.getDoubleArrayTopic("botpose_targetspace").subscribe(new double[6]);
+        botPose = new double[6];
+        botPoseX = 0.0;
+        botPoseZ = 0.0;
+        updates = 0;
     }
 
     public void refreshValues(){
         table = NetworkTableInstance.getDefault().getTable("limelight");
-        botpose = table.getEntry("botpose_targetspace").getDoubleArray(new double[6]);
+        botPose = ySub.get(new double[6]);
+        botPoseX = botPose[0]; //X+ is to the right if you are looking at the tag
+        botPoseZ = botPose[2]; //Z+ is perpendicular to the plane of the tag (Z+ is away from tag on data side, Z- is away on non data side)
+        updates++;
         System.out.println("refresh");
     }
 
     public double getRX(){
         refreshValues();
-        return botpose[0];
+        return botPose[0];
     }
 
     public double getRY(){
         refreshValues();
-        return botpose[1];
+        return botPose[1];
     }
 
     public double getRZ(){
         refreshValues();
-        return botpose[2];
+        return botPose[2];
     }
 
     public double getRoll(){
         refreshValues();
-        return botpose[3];
+        return botPose[3];
     }
 
     public double getPitch(){
         refreshValues();
-        return botpose[4];
+        return botPose[4];
     }
 
     public double getYaw(){
         refreshValues();
-        return botpose[5];
+        return botPose[5];
     }
 
     public Pose3d getPose() {
@@ -81,19 +93,14 @@ public class Limelight extends SubsystemBase{
     }
 
     public Trajectory testTraj(Rotation2d yaw){
-        botposefromtag = table.getEntry("camerapose_targetspace").getDoubleArray(new double[6]);
-        botPose2d[0] = botposefromtag[0]; //X+ is to the right if you are looking at the tag
-        botPose2d[1] = botposefromtag[2]; //Z+ is perpendicular to the plane of the tag (Z+ is away from tag on data side, Z- is away on non data side)
-
-        
         TrajectoryConfig config = new TrajectoryConfig(0.5, 0.5);
         var interiorWaypoints = new ArrayList<Translation2d>();
         interiorWaypoints.add(new Translation2d(0.3, 0.3)); //you can add waypoints like this
+        
         return TrajectoryGenerator.generateTrajectory(
-        new Pose2d(-botPose2d[0], 
-        botPose2d[1], yaw), 
-        interiorWaypoints, 
-        new Pose2d(0, 0, Rotation2d.fromDegrees(0)), 
+        new Pose2d(botPoseZ, botPoseX, yaw),
+        interiorWaypoints,
+        new Pose2d(0.1, 0.1, Rotation2d.fromDegrees(0)), 
         config); //theoretically this path goes to (0,0,0), the pos of the tag, from the robot's location
     }
 
